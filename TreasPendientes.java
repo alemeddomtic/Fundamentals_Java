4c1738e513329229a972f0b22590d83823b917c7
alemeddomtic <143725975+alemeddomtic@users.noreply.github.com>
2023-09-05 09:50:48 -0500

    Add files via upload

---
+++ TreasPendientes.java
@@ -0,0 +1,16 @@
+
+package variables;
+
+
+public class TreasPendientes {
+    public static void main(String[] args) {
+        
+        String ta1 ="Lavar ropa";
+        String ta2= "Dibujar un perrito";
+        String ta3 ="Cocinar ratatui";
+        String ta4= "Colorear mis graficas";
+        String ta5= "Leer el libro de metodos";
+        
+        System.out.println ("Para mi semana debo "+ ta1+" , " + ta2+ " , "+ta3 + " , " +ta4+ " y " +ta5);
+}
+}
