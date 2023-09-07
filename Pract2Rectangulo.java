bd92c9a788fc2e42981ecc07022cf69f8a474d5b
alemeddomtic <143725975+alemeddomtic@users.noreply.github.com>
2023-09-07 09:49:11 -0500

    Add files via upload

---
+++ Pract2Rectangulo.java
@@ -0,0 +1,14 @@
+
+public class Pract2Rectangulo {
+public static void main(String[] args) {
+        
+    int lado= 10;
+    int base= 15;
+    String nombre= "Alexander Medina Dominguez";
+    
+    int arcuad= lado * base;
+    
+    System.out.println ("Mi nombre es " +nombre+ " ,y el area del triangulo es: "+ arcuad); 
+    
+}
+}
